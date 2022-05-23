b30bc96230d162e4ee139bcb6b8860186de80816
Adaszewski <kamilx.adaszewski@intel.com>
2022-05-23 14:28:36 +0200

    3 tabele

--- master1/tabelaPRACOWNICY.sql
+++ master1/tabelaPRACOWNICY.sql
@@ -1,5 +1,19 @@
 CREATE TABLE pracownicy (
     pracownicy_ID int PRIMARY KEY NOT NULL IDENTITY(1,1),
-    
+    pracownik_Imie varchar (20),
+    pracownik_Nazwisko varchar(25),
+    pracownik_Płeć varchar(1),
+    /* M lub K */
+    pracownik_Data_Zatrudnienia DATE,
+    pracownik_Stanowisko varchar(20),
+    pracownik_pensja number,
 
 );
+
+/*
+INSERT INTO pracownicy VALUES('Kamil', 'Adaszewski', 'M', '23.05.2022r', 'Współwłaściciel', 5000);
+INSERT INTO pracownicy VALUES('Dominik', 'Jackowski', 'M', '23.05.2022r', 'Współwłaściciel', 5000);
+INSERT INTO pracownicy VALUES('Jan', 'Kowalski', 'M', '24.05.2022r', 'Magazynier', 3000);
+INSERT INTO pracownicy VALUES('Anna', 'Kowalska', 'K', '24.05.2022r', 'Kasjer', 3000);
+*/
+SELECT * from pracownicy
