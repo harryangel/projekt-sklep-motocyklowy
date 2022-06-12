4793e593c9c63cb156ea13d3241853d6a3c81372
Dominik Jackowski <aekahhzf@gmail.com>
2022-06-12 18:10:28 +0200

    table_zamowienia

--- master1/tabelaZAMOWIENIA.sql
+++ master1/tabelaZAMOWIENIA.sql
@@ -1,12 +1,9 @@
 CREATE TABLE zamowienia (
     zamowienia_ID int PRIMARY KEY NOT NULL IDENTITY(1,1),
-   
     klienci_ID int NOT NULL,
     produkt_ID int NOT NULL,
+	produkt_ilosc int NOT NULL,
     produkt_Cena float NOT NULL,
-    produkt_Marka varchar(25),
-    produkt_Model varchar(100),
-    produkt_Rozmiar varchar(3),
     
     
 
