ea29eac74228e18e6804f83a17d6596bf365acfa
Dominik Jackowski <aekahhzf@gmail.com>
2022-06-17 00:04:00 +0200

    more changes

--- FUNKCJE/sumaceny.sql
+++ FUNKCJE/sumaceny.sql
@@ -7,5 +7,6 @@ BEGIN
 	RETURN (@val1 + @val2)
 END
 
-SELECT sumaceny (szczegoly_Zamowien.produkt_Cena, szczegoly_Zamowien.produkt_Cena)
-where szczegoly_Zamowien.zamowienia_ID = 2
+
+SELECT sumaceny (, szczegoly_Zamowien.produkt_Cena)
+where szczegoly_Zamowien.zamowienia_ID = 2 AND szczegoly_Zamowien.produkt_Cena = @val1
